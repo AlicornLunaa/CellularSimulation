53c1f11278fb069e45b86ee9adad457d207c5782
AlicornLunaa <alicornlunaa@gmail.com>
2021-04-03 16:59:30 -0500

    Worked on electron movement

--- src/main/java/com/AlicornLunaa/CellularSimulation/physics/Physics.java
+++ src/main/java/com/AlicornLunaa/CellularSimulation/physics/Physics.java
@@ -14,7 +14,7 @@ public class Physics {
 
     // Functions
     public void updateElectron(World world, int x, int y){
-        // Move towards protons
+        // Move towards protons and away from electrons
         int radius = 13;
         Cell[] nearby = world.getSurrounding(x, y, radius);
         for(int i = 0; i < nearby.length; i++){
@@ -23,8 +23,20 @@ public class Physics {
             int relX = i / radius;
 
             if(c == null) continue;
+            
+            if(c.getType() == Cell.CellType.ELECTRON){
+                Vector2f direction = new Vector2f(relX - radius / 2, relY - radius / 2);
+                direction.negate();
 
-            if(c.getType() == Cell.CellType.PROTON){
+                // Movement is allowed
+                if (Math.abs(direction.x) > Math.abs(direction.y)) {
+                    // X is greater than the Y, therefore move in the X direction
+                    moves.add(new Move(x, y, x + (int) Math.signum(direction.x), y));
+                } else {
+                    // Y is greater than the X, therefore move in the Y direction
+                    moves.add(new Move(x, y, x, y + (int) Math.signum(direction.y)));
+                }
+            } else if(c.getType() == Cell.CellType.PROTON){
                 Vector2f direction = new Vector2f(relX - radius / 2, relY - radius / 2);
 
                 // Stop movement if less than 2 distance
